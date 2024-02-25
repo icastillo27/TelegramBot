5c063527a7ef657c99551ea7e1dc39c4be50faca
iCastillo <caai.2792@gmail.com>
2024-02-24 20:34:22 -0600

    Initial commit

---
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,27 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "*";
+    public static final String TOKEN = "*";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+sendTextMessageAsync("Hola mundo!!");
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
